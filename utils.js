ad53d6eb6922a2d8113800b4750c393fc2fee694
haralevi <haralevi@users.noreply.github.com>
2016-11-08 22:51:27 +0100

    added function getWndSize

--- utils.js
+++ utils.js
@@ -1,57 +1,66 @@
 var wnd = window, doc = document, docEl = doc.documentElement, body = doc.getElementsByTagName('body')[0];
-
-var waitForFinalEvent = (function () {
-    var timers = {};
-    return function (callback, ms, uniqueId) {
-        if (!uniqueId) {
-            uniqueId = "Don't call this twice without a uniqueId";
+        var wW = 0, hW = 0, wndSize = [];
+        
+        function getWndSize() {
+            wW = wnd.innerWidth || docEl.clientWidth || body.clientWidth;
+            hW = wnd.innerHeight || docEl.clientHeight || body.clientHeight;
+            return [wW, hW];
         }
-        if (timers[uniqueId]) {
-            clearTimeout(timers[uniqueId]);
+
+        var waitForFinalEvent = (function () {
+            var timers = [];
+            return function (callback, ms, uniqueId) {
+                if (!uniqueId) {
+                    uniqueId = "Don't call this twice without a uniqueId";
+                }
+                if (timers[uniqueId]) {
+                    clearTimeout(timers[uniqueId]);
+                }
+                timers[uniqueId] = setTimeout(callback, ms);
+            };
+        })();
+
+        function stopEvent(e) {
+            if (e.preventDefault != undefined)
+                e.preventDefault();
+            if (e.stopPropagation != undefined)
+                e.stopPropagation();
         }
-        timers[uniqueId] = setTimeout(callback, ms);
-    };
-})();
 
-function stopEvent(e) {
-    if (e.preventDefault != undefined)
-        e.preventDefault();
-    if (e.stopPropagation != undefined)
-        e.stopPropagation();
-}
+        doc.oncontextmenu = oncontextmenuOpen;
+        function oncontextmenuOpen(e) {
+            var evt = new Object({keyCode: 93});
+            stopEvent(e);
+            keyboardUp(evt);
+        }
 
-doc.oncontextmenu = oncontextmenuOpen;
-function oncontextmenuOpen(e) {
-    var evt = new Object({keyCode: 93});
-    stopEvent(e);
-    keyboardUp(evt);
-}
+        doc.onkeydown = keyboardDown;
+        function keyboardDown(e) {
 
-doc.onkeydown = keyboardDown;
-function keyboardDown(e) {
+        }
 
-}
+        doc.onkeyup = keyboardUp;
+        function keyboardUp(e) {
 
-doc.onkeyup = keyboardUp;
-function keyboardUp(e) {
+        }
 
-}
+        var container = doc.getElementsByClassName('container');
+        var cellW = 260;
 
-var container = doc.getElementsByClassName('container');
-var cellW = 260;
-function resizeContainer() {
-    var wW = wnd.innerWidth || docEl.clientWidth || body.clientWidth;
-    //var hW = wnd.innerHeight || docEl.clientHeight || body.clientHeight;
-    wW -= 20; //console.log(wW);
-    var containerW = Math.floor(wW / cellW) * cellW;
-    for (var i = 0; i < container.length; i++)
-        container[i].style.maxWidth = containerW + "px";
-}
-resizeContainer();
-
-wnd.onresize = wndResize;
-function wndResize(e) {
-    waitForFinalEvent(function () {
+        function resizeContainer() {
+            wndSize = getWndSize();
+            wW = wndSize[0] - 20;
+            hW = wndSize[1];
+            console.log("width:", wW, "height:", hW);
+            var containerW = Math.floor(wW / cellW) * cellW;
+            for (var i = 0; i < container.length; i++)
+                container[i].style.maxWidth = containerW + "px";
+        }
         resizeContainer();
-    }, 500, 'wnd.onresize');
-}
+
+        wnd.onresize = wndResize;
+        function wndResize(e) {
+            waitForFinalEvent(function () {
+                resizeContainer();
+            }, 300, 'wnd.onresize');
+        }
