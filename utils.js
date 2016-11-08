b7358f46d929491041cbf65ea45a8c77139aceb3
haralevi <my_a_ha@yahoo.com>
2016-11-08 22:53:49 +0100

    formatted

--- utils.js
+++ utils.js
@@ -1,7 +1,14 @@
 var wnd = window, doc = document, docEl = doc.documentElement, body = doc.getElementsByTagName('body')[0];
+var wW = 0, hW = 0, wndSize = [];
+
+function getWndSize() {
+    wW = wnd.innerWidth || docEl.clientWidth || body.clientWidth;
+    hW = wnd.innerHeight || docEl.clientHeight || body.clientHeight;
+    return [wW, hW];
+}
 
 var waitForFinalEvent = (function () {
-    var timers = {};
+    var timers = [];
     return function (callback, ms, uniqueId) {
         if (!uniqueId) {
             uniqueId = "Don't call this twice without a uniqueId";
@@ -39,10 +46,12 @@ function keyboardUp(e) {
 
 var container = doc.getElementsByClassName('container');
 var cellW = 260;
+
 function resizeContainer() {
-    var wW = wnd.innerWidth || docEl.clientWidth || body.clientWidth;
-    //var hW = wnd.innerHeight || docEl.clientHeight || body.clientHeight;
-    wW -= 20; //console.log(wW);
+    wndSize = getWndSize();
+    wW = wndSize[0] - 20;
+    hW = wndSize[1];
+    console.log("width:", wW, "height:", hW);
     var containerW = Math.floor(wW / cellW) * cellW;
     for (var i = 0; i < container.length; i++)
         container[i].style.maxWidth = containerW + "px";
@@ -53,5 +62,5 @@ wnd.onresize = wndResize;
 function wndResize(e) {
     waitForFinalEvent(function () {
         resizeContainer();
-    }, 500, 'wnd.onresize');
+    }, 300, 'wnd.onresize');
 }
