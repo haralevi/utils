ea1235152ff3d2db67061723d186d66cf2c31338
haralevi <my_a_ha@yahoo.com>
2016-11-09 02:34:11 +0100

    initial commit

--- utils.js
+++ utils.js
@@ -1,12 +1,3 @@
-var wnd = window, doc = document, docEl = doc.documentElement, body = doc.getElementsByTagName('body')[0];
-var wW = 0, hW = 0, wndSize = [];
-
-function getWndSize() {
-    wW = wnd.innerWidth || docEl.clientWidth || body.clientWidth;
-    hW = wnd.innerHeight || docEl.clientHeight || body.clientHeight;
-    return [wW, hW];
-}
-
 var waitForFinalEvent = (function () {
     var timers = [];
     return function (callback, ms, uniqueId) {
@@ -20,6 +11,79 @@ var waitForFinalEvent = (function () {
     };
 })();
 
+// Create an immediately invoked functional expression to wrap our code
+(function () {
+
+    // Define our constructor
+    this.Utils = function () {
+
+        // Create global element references
+        this.wnd = window;
+        this.docEl = document.documentElement;
+        this.body = document.getElementsByTagName('body')[0];
+        this.wW = 0;
+        this.hW = 0;
+
+        // Determine proper prefix
+
+        // Define option defaults
+        var defaults = {
+            cellW: 260,
+            container: document.getElementsByClassName('container')
+        };
+
+        // Create options by extending defaults with the passed in arguments
+        if (arguments[0] && typeof arguments[0] === "object") {
+            this.options = extendDefaults(defaults, arguments[0]);
+        }
+
+        // private methods
+        function _getWndSize() {
+            this.wW = this.wnd.innerWidth || this.docEl.clientWidth || this.body.clientWidth;
+            this.hW = this.wnd.innerHeight || this.docEl.clientHeight || this.body.clientHeight;
+        }
+
+        Utils.prototype.fixLayout = function () {
+            _getWndSize.call(this);
+            this.wW -= 20;
+            var containerW = Math.floor(this.wW / this.options.cellW) * this.options.cellW;
+            console.log("width:", this.wW, "height:", this.hW, "ontainerW", containerW);
+            for (var i = 0; i < container.length; i++)
+                container[i].style.maxWidth = containerW + "px";
+        }
+    };
+
+    // Utility method to extend defaults with user options
+    function extendDefaults(source, properties) {
+        var property;
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                source[property] = properties[property];
+            }
+        }
+        return source;
+    }
+}());
+
+var container = document.getElementsByClassName('container');
+var cellW = 260;
+var utils = new Utils({
+    cellW: cellW,
+    container: container
+});
+
+utils.fixLayout();
+
+window.onresize = wndResize;
+function wndResize() {
+    waitForFinalEvent(
+        function () {
+            utils.fixLayout();
+        },
+        300,
+        'wnd.onresize');
+}
+
 function stopEvent(e) {
     if (e.preventDefault != undefined)
         e.preventDefault();
@@ -27,43 +91,20 @@ function stopEvent(e) {
         e.stopPropagation();
 }
 
-doc.oncontextmenu = oncontextmenuOpen;
+document.oncontextmenu = oncontextmenuOpen;
 function oncontextmenuOpen(e) {
     var evt = new Object({keyCode: 93});
     stopEvent(e);
     keyboardUp(evt);
 }
 
-doc.onkeydown = keyboardDown;
+document.onkeydown = keyboardDown;
 function keyboardDown(e) {
 
 }
 
-doc.onkeyup = keyboardUp;
+document.onkeyup = keyboardUp;
 function keyboardUp(e) {
 
 }
 
-var container = doc.getElementsByClassName('container');
-var cellW = 260;
-
-function fixLayout() {
-    wndSize = getWndSize();
-    wW = wndSize[0] - 20;
-    hW = wndSize[1];
-    console.log("width:", wW, "height:", hW);
-    var containerW = Math.floor(wW / cellW) * cellW;
-    for (var i = 0; i < container.length; i++)
-        container[i].style.maxWidth = containerW + "px";
-}
-fixLayout();
-
-wnd.onresize = wndResize;
-function wndResize(e) {
-    waitForFinalEvent(
-        function () {
-            fixLayout();
-        },
-        300,
-        'wnd.onresize');
-}
