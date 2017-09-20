633dbf0ccee66a5a3059970a9db71998caff056b
haralevi <a.haralevich@gmail.com>
2017-09-20 19:39:10 +0200

    functional js example

---
+++ functional/functional.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Functional</title>
+</head>
+<body>
+
+<div id="root"></div>
+
+<script>
+	const root = document.getElementById("root");
+	
+    const log = o => console.log(o);
+
+    const map = fn => array => array.map(fn);
+    const multiply = x => y => x * y;
+    const pluck = key => object => object[key];
+
+    const discount = multiply(0.98);
+    const tax = multiply(1.0925);
+
+    const url = 'prices.php';
+
+    const request = defaults => async options => {
+        options = Object.assign({}, defaults, options);
+        const resp = await fetch(options.url, options.headers);
+		return resp.json();
+    };
+
+    const customRequest = request({
+    	headers: {
+    		'method': 'POST',
+    		'X-Custom': 'mykey'
+    	}
+    });
+
+    const resp = customRequest({ url: url });
+    
+    resp.then(map(pluck('price')))
+		.then(map(discount))
+		.then(map(tax))
+		.then(prices => printPrices(prices));
+   	
+	const printPrices = (prices) => {
+		
+		prices.map((price) => {
+			const p = document.createElement("p");
+			const t = document.createTextNode(price.toFixed(3));
+			p.appendChild(t);
+			root.appendChild(p);
+		});
+	};
+
+    log('ok');
+</script>
+</body>
+</html>
---
+++ functional/prices.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+$json = '[
+	{ "price": 5},
+	{ "price": 10},
+	{ "price": 3}
+]';
+
+echo $json;
